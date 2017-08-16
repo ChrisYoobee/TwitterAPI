4dddd3f732e1ca639935e4ee070ae77bb5a971d1
christopher gary hughes <845869606@ken20-202-06.local>
2017-08-17 10:07:08 +1200

    Commit 2

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var express = require("express");
-var cors = require("cors");
+var express = require('express');
+var cors = require('cors');
+var Twit = require('twit');
+var config = require('./config')
+
+var T = new Twit({
+  consumer_key:         config.consumer_key:,
+  consumer_secret:      config.consumer_secret,
+  access_token:         config.access_token,
+  access_token_secret:  config.access_token_secret,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+})
 
 var app = express();
 
